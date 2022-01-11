0b1bfff18287105c4cd6009a4d394c0e182ebf23
banchi <icaro_19@hotmail.com>
2022-01-12 00:41:34 +0100

    modify src/App.js

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,18 +5,7 @@ function HomePage() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Casaverde</h1>
       </header>
     </div>
   );
